84eea382da4ab41cd46fa6448674917df7bc9134
ANDERS L KOLSTAD <anders.kolstad@ntnu.no>
2018-02-09 15:01:16 +0100

    blabla

--- PRC.R
+++ PRC.R
@@ -30,6 +30,7 @@
 library(readr)
 library(readxl)
 library(dplyr)
+library(ggplot2)
 
 setwd("M:\\Anders L Kolstad\\R\\R_projects\\succession_paper")
 cdat <- read_excel("community_data.xlsx", 
@@ -61,6 +62,9 @@ yse <- ifelse(cdat$Region == "Trøndelag", as.numeric(as.character(cdat$year))-2
 cdat <- cbind(yse, cdat)
 #plot(cdat$yse)
 table(cdat$Region, cdat$yse)
+
+table(cdat$LocalityName, cdat$Plot)
+cdat$Plot <- ifelse(cdat$Plot > 10, cdat$Plot-10, cdat$Plot)
 
 
 # remove some columns that are not vascular plant species
@@ -163,10 +167,10 @@ cdat <- cdat2;rm(cdat2)
 # remove thinned plots
 loc_year <- interaction(cdat$LocalityName, cdat$yse)
 cdat <- cbind(loc_year, cdat)
-unique(cdat$loc_year)
-thinned <- c("Selbu_Flub.8", "Malvik.8", "Hi_tydal.8")
-cdat2 <- cdat[!cdat$loc_year %in% thinned,]
-cdat <- cdat2;rm(cdat2)
+#unique(cdat$loc_year)
+#thinned <- c("Selbu_Flub.8", "Malvik.8", "Hi_tydal.8")
+#cdat2 <- cdat[!cdat$loc_year %in% thinned,]
+#cdat <- cdat2;rm(cdat2)
 
 
 
@@ -175,7 +179,7 @@ cdat <- cdat2;rm(cdat2)
 # Keeping Avenella and Des Cea as they are. 
 # Carex is grouped together. 
 
-# Luzula is grouped
+
 
 
 sub_other_graminoids2 <- c("Anthoxanthum odoratum",
@@ -337,8 +341,7 @@ SH_taxa <- SH_taxa2[SH_taxa2 %in% names(cdat)]
 
 BLG_taxa2 <- c("Deschampsia cespitosa", 
               sub_other_graminoids[!sub_other_graminoids == c("Nardus stricta", "Eriophorum vaginatum")],
-              carex, 
-              luzula )
+              carex  )
 BLG_taxa <- BLG_taxa2[BLG_taxa2 %in% names(cdat)]
 
 NLG_taxa <- c("Avenella flexuosa",
@@ -369,8 +372,6 @@ ferns_fg <- c("Athyrium filix-femina",
 
 cdatBM <- cdat   
 cdatBM[,16:ncol(cdatBM)] <- NULL
-# check for zeros. They should be NAs:
-#cdatBM[cdatBM == 0] <- NA
 
 cdat[,16:ncol(cdat)] <- cdat[,16:ncol(cdat)]/16   # standardising the intercept frequency
 
@@ -389,67 +390,376 @@ which.min(colSums(cdatBM[,16:ncol(cdatBM)], na.rm=T))
 #which(colSums(cdatBM[,16:ncol(cdatBM)], na.rm=T) == 0)
 
 
+# combining species that are hard to identify in the field
+cdatBM2 <- cdatBM
+cdatBM$other_graminoids <- rowSums(cdatBM[, sub_other_graminoids], na.rm=T)
+cdatBM$Carex <- rowSums(cdatBM[, carex], na.rm=T)
+cdatBM$Epilobium <-  rowSums(cdatBM[, epilobium], na.rm=T)
+cdatBM$Alchemilla <- rowSums(cdatBM[, alchemilla], na.rm=T)
+cdatBM$Galeopsis <-  rowSums(cdatBM[, galeopsis], na.rm=T)
+cdatBM2 <- cdatBM[,!colnames(cdatBM) %in% c(carex, epilobium, alchemilla, galeopsis)]
+cdatBM <- cdatBM2;rm(cdatBM2)
+
+# removing singletons and species rarer than 10%
+# total nr location:year = 155 (-3)
+#length(unique(cdatBM$loc_year))
+# define cut of at 15
+
+cdatBM2 <- cdatBM
+cdatBM <- cdatBM[,c(rep(TRUE, times=15), colSums(cdatBM[,16:ncol(cdatBM)] >1, na.rm=T) >  15)]
+rm(cdatBM2)
+
+# Functional groups ####
+FG <- cdatBM
+grasses <- c("other_graminoids",
+             "Carex",
+             "Avenella flexuosa",
+             "Deschampsia cespitosa")
+FG$grasses <- rowSums(FG[,names(FG) %in% grasses], na.rm=TRUE)
+large_herbs <- TH_taxa[TH_taxa != ("Veronica officinalis")]
+large_herbs <- large_herbs[!large_herbs %in% ferns]
+FG$large_herbs <- rowSums(FG[,names(FG) %in% large_herbs], na.rm=TRUE)
+small_herbs <- c(SH_taxa[SH_taxa %in% names(cdatBM)], "Epilobium", "Alchemilla", "Galeopsis")
+FG$small_herbs <- rowSums(FG[,names(FG) %in% small_herbs], na.rm=TRUE)
+ferns <- ferns_fg[ferns_fg %in% names(cdatBM)]
+FG$ferns <- rowSums(FG[,names(FG) %in% ferns], na.rm=TRUE)
+shrubs_fg <- shrubs_fg[shrubs_fg %in% names(cdatBM)]
+FG$shrubs <- rowSums(FG[,names(FG) %in% shrubs_fg], na.rm=TRUE)
+library(reshape2)
+FG2 <- select(FG, yse, Region, LocalityName, Treatment, Plot,
+              grasses, large_herbs, small_herbs,
+              ferns, shrubs)
+FG3 <- melt(data = FG2, id.vars = names(FG2[,1:5]), measure.vars = names(FG2[,6:ncol(FG2)]))
+names(FG3)[names(FG3) == "variable"] <- "group"
+names(FG3)[names(FG3) == "value"]    <- "biomass"
+
+FG4 <- aggregate(data=FG3, biomass~yse+Treatment+group, FUN = mean)
+
+ggplot(data = FG4, aes(x=yse, y= biomass, group=group, colour=group))+
+  geom_line()+theme_bw()+
+  facet_grid(. ~ Treatment, scales = "fixed")
+
+FG5 <- dcast(FG4, yse+group~Treatment, value.var = "biomass", fun.aggregate = mean)
+FG5$diff <- FG5$UB-FG5$B
+ggplot(data = FG5, aes(x=yse, y= diff, group=group, colour=group, linetype= group))+
+  geom_line(size=2)+theme_bw()+geom_hline(yintercept = 0, size=2)+
+  xlab("Years since exclosure")+ylab("Grams of biomass (Exclosure - open plots)")+
+  theme(legend.position="top")+
+  theme(legend.title=element_blank())
+  
+FG6 <- aggregate(data=FG3, biomass~yse+LocalityName+Treatment+group, FUN = mean)
+FG6 <- dcast(FG6, yse+group+LocalityName~Treatment, value.var = "biomass", fun.aggregate = mean)
+FG6$diff <- FG6$UB-FG6$B
+exl <- rownames(FG6[FG6$group=="ferns" & FG6$diff >80,])
+FG7 <- FG6[!rownames(FG6) %in% exl,]
+
+#tiff("Functional_groups_facet.tiff", height = 10, width = 10, units = "cm", res=300)
+ggplot(data = FG7, aes(x=yse, y= diff))+
+  geom_point(size=2)+
+  theme_bw()+
+  geom_hline(yintercept = 0, size=2)+
+  geom_smooth(method = "lm")+
+  xlab("Years since exclosure")+ylab("Grams of biomass (Exclosure - open plots)")+
+  theme(legend.position="top")+
+  theme(legend.title=element_blank())+
+  facet_wrap( ~ group, scales = "free")
+#dev.off()
+
+#tiff("Functional_groups_onePlot.tiff", height = 10, width = 10, units = "cm", res=300)
+ggplot(data = FG7, aes(x=yse, y= diff, group = group, colpur = group))+
+  theme_bw()+
+  geom_hline(yintercept = 0, size=2)+
+  geom_smooth(method = "lm", se = F, size = 3, aes(colour = group))+
+  xlab("Years since exclosure")+ylab("Grams of biomass (Exclosure - open plots)")+
+  theme(legend.position="top")+
+  theme(legend.title=element_blank())
+#dev.off()  
+
+
+# Analyse functional groups   ####
+library(glmmTMB)
+TH_glm <- glmmTMB(biomass+0.1 ~ Treatment * yse + (1|Region/LocalityName/Plot), 
+                  data = subset(FG3, group == "large_herbs"), family = Gamma(link = "log"))
+library(lme4)
+TH_glm <- lmer(biomass ~ Treatment * yse + (1|Region/LocalityName/Plot), 
+                  data = subset(FG3, group == "large_herbs"))
+library(nlme)
+TH_glm <- lme(log(biomass+1) ~ Treatment * yse, random = ~1|Region/LocalityName/Plot, 
+               data = subset(FG3, group == "large_herbs"))
+
+summary(TH_glm)
+plot(TH_glm)
+qqnorm(resid(TH_glm))
+
+#not working well
+# try ignoring time (drawback is you cant standarize by year 0, 
+# unless you subtract the biomass in year 0, but year 0 is still a few months after the fence can up,
+# which is important for short lived species.
+THdat <- subset(FG3, group == "large_herbs" & yse %in% c(6,8))
+THdat2 <- subset(FG3, group == "large_herbs")
+
+sum(THdat$biomass==0)/nrow(THdat)
+# way too many zero - aggregating to plot level
+THdat <- aggregate(data=THdat, biomass~Region+LocalityName+Treatment+group, FUN = mean)
+THdat2 <- aggregate(data=THdat2, biomass~Region+LocalityName+Treatment+group+yse, FUN = mean)
+sum(THdat$biomass==0)/nrow(THdat) #ok
+
+TH_glm <- lme(log(biomass+1) ~ Treatment, random = ~1|Region/LocalityName, 
+              data = THdat)
+TH_glm <- lme(log(biomass+1) ~ Treatment*yse, random = ~1|Region/LocalityName, 
+              data = THdat2) # negative fitted values
+
+TH_glm <- glmmTMB(biomass+0.1 ~ Treatment*yse + (1|Region/LocalityName), 
+                  data = THdat2,
+                  family = Gamma(link = "log"))
+
+TH_glm <- glmer(biomass+0.1 ~ Treatment*scale(yse, scale=F) +(1|Region/LocalityName), 
+                  data = THdat2,
+                  family = Gamma)
+
+summary(TH_glm)
+plot(TH_glm)
+R <- resid(TH_glm, type = "pearson")
+plot(fitted(TH_glm), R)
+boxplot(log(THdat$biomass+1)~as.factor(THdat$Treatment))
+boxplot(R~as.factor(THdat2$Treatment))
+boxplot(R~as.factor(THdat$Treatment))
+
+
+# tHIS WORKS BEST, EVENT WITH A FEW NEGATIVE FITTED VALUES
+TH_glm <- lme(log(biomass+1) ~ Treatment*yse, random = ~1|Region/LocalityName, 
+              data = THdat2) # negative fitted values
+
+
 
 
 # PRC
 library(vegan)
-# combining species that are hard to identify in the field
-PRCdat <- cdatBM
-PRCdat$other_graminoids <- rowSums(PRCdat[, sub_other_graminoids], na.rm=T)
-PRCdat$Carex <- rowSums(PRCdat[, carex], na.rm=T)
-PRCdat$Epilobium <-  rowSums(PRCdat[, epilobium], na.rm=T)
-PRCdat$Alchemilla <- rowSums(PRCdat[, alchemilla], na.rm=T)
-PRCdat$Galeopsis <-  rowSums(PRCdat[, galeopsis], na.rm=T)
-PRCdat2 <- PRCdat[,!colnames(PRCdat) %in% c(carex, epilobium, alchemilla, galeopsis)]
-PRCdat <- PRCdat2;rm(PRCdat2)
 
-PRCdat2 <- PRCdat
-PRCdat[is.na(PRCdat)] <- 0
-rm(PRCdat2)
+cdatBM[is.na(cdatBM)] <- 0
+cdatx <- cdatBM
+library(reshape2)
+long <- melt(data = cdatx, id.vars = c("yse", "Region", "LocalityName", "Treatment", "Plot") , 
+             measure.vars = names(cdatx[,16:ncol(cdatx)]))
+names(long)[names(long) == "variable"] <- "taxa"
+names(long)[names(long) == "value"]    <- "biomass"
+wide <- dcast(data = long, yse + Region + LocalityName + Treatment ~ taxa, 
+              value.var = "biomass", fun.aggregate = mean)
+wide2 <- dcast(data = long, yse + Region + LocalityName + Treatment+Plot ~ taxa, 
+              value.var = "biomass", fun.aggregate = mean)
+thinned <- c("Selbu_Flub", "Hi_tydal", "Malvik")
+unique(wide2$LocalityName)
+wide2_red <- wide2[!wide2$LocalityName %in% thinned,]
+
+
+
+
+
+# PRC on plot level data
+prc.out <- prc(log(wide[,5:ncol(wide)]+1), 
+                      as.factor(wide$Treatment), 
+                      as.factor(wide$yse))
+
+sum_mod_prc <- summary(prc.out, scaling = 0)
+#species <- colSums(rda.res)
+par(mar=c(5,5,10,10))
+prc.out
+#summary(prc.out)
+plot(prc.out, select = abs(sum_mod_prc$sp) > 0.1, scaling = 0)
+ctrl <- how(plots = Plots(wide$LocalityName),
+            within = Within(type = "series"), nperm = 500)
+anova(prc.out, permutations = ctrl, first=TRUE)
+# not significant 
+
+# compare # PRC on SUBplot level data
+prc.out.sub <- prc(log(wide2[,6:ncol(wide2)]+1), 
+               as.factor(wide2$Treatment), 
+               as.factor(wide2$yse))
+prc.out.sub # inertia very low - ignoring locations and plot level variation here
+sum_mod_prc_sub <- summary(prc.out.sub, scaling = 2)
+#species <- colSums(rda.res)
+par(mar=c(5,5,10,10))
+
+
+#tiff("PRC.tiff", height = 10, width = 10, units = "cm", res=300)
+plot(prc.out.sub, select = abs(sum_mod_prc_sub$sp) > 0.12, scaling = 2, 
+     legpos = NULL, xlab = "Year since exclosure")
+dev.off()
+# results comparable
+
+
+# compare removing thinned sites
+prc.out.sub2 <- prc(log(wide2_red[,6:ncol(wide2_red)]+1), 
+                   as.factor(wide2_red$Treatment), 
+                     as.factor(wide2_red$yse))
+prc.out.sub2 # inertia very low - ignoring locations and plot level variation here
+sum_mod_prc_sub2 <- summary(prc.out.sub2, scaling = 2)
+#species <- colSums(rda.res)
+par(mar=c(5,5,10,10))
+
+plot(prc.out.sub2, select = abs(sum_mod_prc_sub2$sp) > 0.12, scaling = 2, 
+     legpos = NULL, xlab = "Year since exclosure")
+# results comparable
+
+## Locations are randomized, we have a time series, and are only
+## interested in the first axis
+ctrl <- how(plots = Plots(wide2$LocalityName),
+            within = Within(type = "series"), nperm = 500    )
+anova(prc.out.sub, permutations = ctrl, first=TRUE)
+
+# compare with partial PRC method on aggregated data:
+rda.out <- rda(log(wide[,5:ncol(wide)]+1) ~ wide$LocalityName) # equivalent of "rda.out = rda(X ~ Condition(Z))"
+rda.out # location explains 70%
+# unconstrained axes  all have equally high eigenvalues!
+rda.res <- residuals(rda.out)    # does this carry only the residuals from the first axis?
+
+prc.out2 <- prc(rda.res, as.factor(wide$Treatment), 
+               as.factor(wide$yse))
+anova(prc.out2)
+sum_mod_prc2 <- summary(prc.out, scaling = 2)
+#species <- colSums(rda.res)
+par(mar=c(5,5,10,10))
+prc.out2
+plot(prc.out2, select = abs(sum_mod_prc2$sp) > 0.1, scaling = 2)
+
+
+# compare with partial PRC method on plot level data:
+rda.out2 <- rda(log(wide2[,6:ncol(wide2)]+1) ~ wide2$LocalityName) # equivalent of "rda.out = rda(X ~ Condition(Z))"
+rda.out2 # location explains 34%
+plot(rda.out2)
+rda.res2 <- residuals(rda.out2)    # does this carry only the residuals from the first axis?
+
+prc.out3 <- prc(rda.res2, as.factor(wide2$Treatment), 
+                as.factor(wide2$yse))
+sum_mod_prc3 <- summary(prc.out3, scaling = 2)
+#species <- colSums(rda.res)
+par(mar=c(5,5,10,10))
+prc.out2 # time: 11%; treatment = 2%; residual variance = 87%
+plot(prc.out3, select = abs(sum_mod_prc3$sp) > 0.05, scaling = 2)
+# all methods look similar
+
+
 
 
 
 # plot time series ####
-library(reshape2)
-long <- melt(data = PRCdat, id.vars = c("yse", "Region", "LocalityName", "Treatment", "Plot") , measure.vars = names(PRCdat[,16:ncol(PRCdat)]))
-names(long)[names(long) == "variable"] <- "taxa"
-names(long)[names(long) == "value"]    <- "biomass"
 
-long$cyse <- as.character(long$yse)
+#long$cyse <- as.character(long$yse)
 # get means per plot and year
 timeS <- aggregate(data = long, 
-                   biomass~Region+cyse+LocalityName+Treatment+taxa, FUN = mean)
-timeS2 <- aggregate(data = timeS, 
-                   biomass~cyse+Treatment+taxa, FUN = mean)
+                   biomass~Region+yse+LocalityName+Treatment+taxa, FUN = mean)
+timeS$biomass_l <- log(timeS$biomass+1)
+#timeS2 <- aggregate(data = timeS, 
+#                   biomass~cyse+Treatment+taxa, FUN = mean)
+timeS3 <- dcast(data = timeS, yse+taxa+LocalityName~Treatment, value.var = "biomass_l", fun.aggregate = mean)
+timeS3$diff <- timeS3$UB-timeS3$B 
+timeS3 <- timeS3[timeS3$diff!=0,]
+
+pos_sp <- c(#"Calluna vulgaris",
+        "Epilobium angustifolium",
+        "Filipendula ulmaria",
+        "Dryopteris expansa")
+        #"Rubus idaeus",
+        #"Gymnocarpium dryopteris",
+        #"Vaccinium uliginosum",
+        #"Pteridium aquilinum")
+neg_sp <- c(#"Vaccinium vitis-idaea",
+            "Vaccinium myrtillus",
+            "Potentilla erecta",
+            "Avenella flexuosa")
+            #"Chamaepericlymenum suecicum",
+            #"Melampyrum sylvaticum")
+library(gridExtra)
+grid.arrange(
+ggplot(data = timeS3[timeS3$taxa %in% pos_sp,], aes(x=yse, y=diff))+
+  geom_point()+
+  geom_smooth(method = "loess")+
+  theme_bw()+
+  geom_hline(yintercept = 0, size = 2)+
+  facet_grid(. ~ taxa, scales = "fixed"),
+ggplot(data = timeS3[timeS3$taxa %in% neg_sp,], aes(x=yse, y=diff))+
+  geom_point()+
+  geom_smooth(method = "loess")+
+  theme_bw()+
+  geom_hline(yintercept = 0, size = 2)+
+  facet_grid(. ~ taxa, scales = "fixed"), nrow=2)
+
+ggplot(data = timeS[timeS$taxa=="Vaccinium myrtillus",], aes(x=cyse, y=biomass, group = LocalityName))+
+  geom_line()+
+  theme_classic()+
+  facet_grid(. ~ Treatment+Region, scales = "fixed")
+
+ggplot(data = timeS[timeS$taxa=="Vaccinium vitis-idaea",], aes(x=yse, y=biomass))+
+  theme_bw()+
+  geom_smooth()+
+  geom_smooth(method = "lm")+
+  geom_point()+
+  facet_grid(. ~ Treatment, scales = "fixed")
+ggplot(data = timeS[timeS$taxa=="Calluna vulgaris",], aes(x=yse, y=biomass))+
+  theme_bw()+
+  geom_smooth()+
+  geom_smooth(method = "lm")+
+  geom_point()+
+  facet_grid(. ~ Treatment, scales = "fixed")
+ggplot(data = timeS[timeS$taxa=="Vaccinium myrtillus",], aes(x=yse, y=biomass))+
+  theme_classic()+
+  geom_smooth()+
+  geom_smooth(method = "lm")+
+  geom_point()+
+  facet_grid(. ~ Treatment, scales = "fixed")
+
+
 ggplot(data = timeS2, aes(x=cyse, y=biomass, group = taxa))+
   geom_line()+
   theme_classic()+
   facet_grid(. ~ Treatment, scales = "fixed")
-#outlier in year4?
+#outlier in year4? - Vac.myr
+#plot(timeS2$biomass)
+#identify(timeS2$biomass)
+#head(timeS2)
+ggplot(data = timeS[timeS$taxa=="Vaccinium myrtillus",], aes(x=cyse, y=biomass, group = LocalityName))+
+  geom_line()+
+  theme_classic()+
+  facet_grid(. ~ Treatment+Region, scales = "fixed")
+# I don't think it a mistake - same pattern for both regions and treatments
+
+ggplot(data = timeS[timeS$taxa=="Calluna vulgaris",], aes(x=cyse, y=biomass, group = LocalityName))+
+  geom_line()+
+  theme_classic()+
+  facet_grid(. ~ Region+Treatment, scales = "fixed")
+# Calluna increases all over - more common in Telemark
 
 
-
-timeS3 <- dcast(data = timeS2, cyse+taxa~Treatment, value.var = "biomass", fun.aggregate = mean)
-timeS3$diff <- timeS3$UB-timeS3$B 
 
 library(ggplot2)
 ggplot(data = timeS3, aes(x=cyse, y=diff, group = taxa))+
          geom_line()+
   theme_classic()
-
-# label the most extreme species
-diff_sp <- unique(timeS3$taxa[timeS3$diff >0.2])
-# calluna is extreme - investigate!
-
+ggplot(data = timeS3[timeS3$taxa=="Vaccinium myrtillus",], aes(x=cyse, y=diff, group = taxa))+
+  geom_line()+
+  theme_classic()
 
 
+
+#  the most extreme species
+diff_sp <- unique(timeS3$taxa[timeS3$diff >0.05])
+# calluna is extreme - investigate! 
+
+timeS3[which.max(timeS3$diff),]
 
 
 
 # taking out the localitions effect
+PRCdat <- cdatBM
+PRCdat2 <- PRCdat
+PRCdat[,16:ncol(PRCdat)] <- scale(PRCdat[,16:ncol(PRCdat)])
+summary(PRCdat2[,16:30])
+summary(PRCdat[,16:30])
+
+
 rda.out <- rda(PRCdat[,16:ncol(PRCdat)] ~ PRCdat$LocalityName) # equivalent of "rda.out = rda(X ~ Condition(Z))"
 rda.out # location explains 31%
-rda.res <- residuals(rda.out)
+rda.res <- residuals(rda.out)    # does this carry only the residuals from the first axis?
 plot(rda.out)
 
 prc.out <- prc(rda.res, as.factor(PRCdat$Treatment), 
@@ -457,7 +767,8 @@ prc.out <- prc(rda.res, as.factor(PRCdat$Treatment),
 sum_mod_prc <- summary(prc.out, scaling = 2)
 #species <- colSums(rda.res)
 par(mar=c(5,5,10,10))
-plot(prc.out, select = abs(sum_mod_prc$sp) > 0.1, scaling = 2)
+prc.out
+plot(prc.out, select = abs(sum_mod_prc$sp) > 0.03, scaling = 2)
 
 
 prc.out.region <- prc(PRCdat[,16:ncol(PRCdat)], as.factor(PRCdat$Region), 
@@ -469,3 +780,29 @@ prc.out.location <- prc(PRCdat[,16:ncol(PRCdat)], as.factor(PRCdat$LocalityName)
                       as.factor(PRCdat$yse))
 sum_mod_prc3 <- summary(prc.out.location, scaling = 2)
 plot(prc.out.location, select = abs(sum_mod_prc3$sp)>0.1, scaling = 2, legpos = NA)
+
+
+
+
+
+# OLD ####
+# standardizing time effect from year 0
+wide3 <- dcast(data = long, Region + LocalityName + Treatment+Plot+taxa ~ yse, 
+               value.var = "biomass", fun.aggregate = mean)
+yrs <- c("2", "4", "6", "8")
+wide3.2 <- wide3
+for(i in yrs){
+  wide3[,i] <- wide3[,i]-wide3$`0`
+}
+
+wide3 <- wide3[ , !(names(wide3) %in% c("0"))]
+
+wide3 <- melt(data = wide3, 
+              id.vars = c("Region", "LocalityName", "Treatment", "Plot", "taxa"),
+              measure.vars = yrs)
+names(wide3)[names(wide3) == "variable"] <- "yse"
+names(wide3)[names(wide3) == "value"]    <- "biomass"
+wide3 <- dcast(data = wide3, yse + Region + LocalityName + Treatment+Plot ~ taxa, 
+               value.var = "biomass", fun.aggregate = mean)
+
+wide3[is.na(wide3)] <- 0
